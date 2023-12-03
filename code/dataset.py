7e4e98585f39762195e61e5745acfab69fca2189
manedavtyan <mane_davtyan@edu.aua.am>
2023-12-03 15:11:47 +0400

    data preprocessing

--- code/dataset.py
+++ code/dataset.py
@@ -1,116 +1,36 @@
-#Includes all code related to the dataset,
-# such as the dataset class, preprocessing,
-# augmentation, and post-processing routines.
+# #Includes all code related to the dataset,
+# # such as the dataset class, preprocessing,
+# # augmentation, and post-processing routines.
+#
 
-import copy
 import os
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import os
-import seaborn as sns
-import skimage
-from skimage import io, transform
-from sklearn.metrics import confusion_matrix
 import torch
-import torch.nn as nn
-import torch.optim as optim
-import torchvision
-from torchvision import datasets, models, transforms
-
-EPOCHS = 30
-data_dir = "..//data"
-TEST = 'test'
-TRAIN = 'train'
-VAL ='val'
-
-
+from torchvision import datasets, transforms
 
 def data_transforms(phase):
-    # If the phase is TRAIN
-    if phase == TRAIN:
-        # Compose transformations: Resize, CenterCrop, Convert to Tensor, Normalize
-        transform = transforms.Compose([
-            transforms.Resize(256),  # Resize the image to 256x256 pixels
-            transforms.CenterCrop(224),  # Crop the center of the image to 224x224 pixels
-            transforms.ToTensor(),  # Convert the image to a PyTorch Tensor
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),  # Normalize the Tensor
-        ])
-
-    # If the phase is VAL
-    if phase == VAL:
-        # Apply the same transformations as the TRAIN phase
+    if phase == 'train':
         transform = transforms.Compose([
             transforms.Resize(256),
             transforms.CenterCrop(224),
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
         ])
-
-    # If the phase is TEST
-    if phase == TEST:
-        # Apply the same transformations as the TRAIN phase
+    elif phase == 'val' or phase == 'test':
         transform = transforms.Compose([
             transforms.Resize(256),
             transforms.CenterCrop(224),
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
         ])
+    return transform
 
-    return transform  # Return the composed transformations
+def load_datasets_and_dataloaders(data_dir):
+    image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms(x)) for x in ['train', 'val', 'test']}
+    dataloaders = {
+        'train': torch.utils.data.DataLoader(image_datasets['train'], batch_size=4, shuffle=True),
+        'val': torch.utils.data.DataLoader(image_datasets['val'], batch_size=1, shuffle=True),
+        'test': torch.utils.data.DataLoader(image_datasets['test'], batch_size=1, shuffle=True)
+    }
 
-
-# Check if CUDA is available and set the device accordingly
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-print(device)  # Print the device being used
-
-
-
-# Create a dictionary of datasets for each phase (TRAIN, VAL, TEST)
-# For each phase, load the images from the corresponding folder and apply the data transformations
-image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms(x))
-                  for x in [TRAIN, VAL, TEST]}
-
-# Create a dictionary of dataloaders for each phase
-# For each phase, create a DataLoader that loads the data from the corresponding dataset
-# The batch size and shuffle parameters can be adjusted as needed
-dataloaders = {
-    TRAIN: torch.utils.data.DataLoader(image_datasets[TRAIN], batch_size = 4, shuffle=True),
-    VAL: torch.utils.data.DataLoader(image_datasets[VAL], batch_size = 1, shuffle=True),
-    TEST: torch.utils.data.DataLoader(image_datasets[TEST], batch_size = 1, shuffle=True)
-}
-
-
-
-# Get the sizes of the datasets
-dataset_sizes = {
-    x: len(image_datasets[x])
-    for x in [TRAIN, VAL]
-}
-
-# Get the classes from the training dataset
-classes = image_datasets[TRAIN].classes
-
-# Get the class names from the training dataset
-class_names = image_datasets[TRAIN].classes
-
-# plotting the pictures
-fig, axes = plt.subplots(6, 6, figsize=(12, 12))
-fig.subplots_adjust(hspace=0.3, wspace=0.3)
-
-for i in range(6):
-    for j in range(6):
-        inputs, classes = next(iter(dataloaders[TRAIN]))
-        input_img = inputs[0]
-        class_label = classes[0]
-        inp = input_img.numpy().transpose((1, 2, 0))
-        mean = np.array([0.485, 0.456, 0.406])
-        std = np.array([0.229, 0.224, 0.225])
-        inp = std * inp + mean
-        inp = np.clip(inp, 0, 1)
-        axes[i, j].imshow(inp)
-        axes[i, j].set_title(class_names[class_label.item()])
-        axes[i, j].axis('off')
-
-plt.show()
+    return dataloaders
 
