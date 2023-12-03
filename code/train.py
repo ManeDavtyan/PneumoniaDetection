7f227c385193f52fb1ea66f6949d06dde13f36f3
manedavtyan <mane_davtyan@edu.aua.am>
2023-12-03 15:12:37 +0400

    training the model

--- code/train.py
+++ code/train.py
@@ -1,3 +1,91 @@
 #Contains the training codes.
 # It must accept a dataset path and hyperparameters as inputs.
 # It should produce and save at least one checkpoint as output.
+
+
+# train.py
+import copy
+import torch
+import torch.nn as nn
+import torch.optim as optim
+# train.py
+import copy
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import models
+from dataset import load_datasets_and_dataloaders
+
+def train_model(model, dataloaders, criterion, optimizer, scheduler, num_epochs):
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print("Epoch: {}/{}".format(epoch + 1, num_epochs))
+        print("=" * 10)
+
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                scheduler.step()
+                model.train()
+            else:
+                model.eval()
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                optimizer.zero_grad()
+
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / len(dataloaders[phase].dataset)
+            epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+                # Save the best model checkpoint
+                torch.save(best_model_wts, 'best_model.pth')
+
+    print('Best val Acc: {:.4f}'.format(best_acc))
+
+    # Save the final model checkpoint
+    torch.save(model.state_dict(), 'final_model.pth')
+
+    model.load_state_dict(best_model_wts)
+    return model
+
+if __name__ == "__main__":
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    # Modify paths and parameters as needed
+    data_dir = "..//data"
+    dataloaders = load_datasets_and_dataloaders(data_dir)
+
+    model = models.vgg16(pretrained=True)
+    num_features = model.classifier[6].in_features
+    model.classifier[6] = nn.Linear(num_features, 2)  # Assuming binary classification
+
+    model = model.to(device)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9, weight_decay=0.01)
+    exp_lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.1)
+
+    model = train_model(model, dataloaders, criterion, optimizer, exp_lr_scheduler, num_epochs=30)
