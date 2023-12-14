78c4eed7368d9b89af39b859d5f9a005c401e7ca
Mane Davtyan <79759934+ManeDavtyan@users.noreply.github.com>
2023-12-14 18:54:25 +0400

    Add files via upload

--- code/inference.py
+++ code/inference.py
@@ -1,8 +1,8 @@
-#Contains the code inference code for processing a single image.
-#It should take a single image path as input and save the output on Result folder.
+# # # Contains the code inference code for processing a single image.
+# # # It should take a single image path as input and save the output on Result folder.
 
 
-# inference.py
+
 import torch
 from torchvision import transforms
 from PIL import Image
@@ -37,15 +37,15 @@ def inference_single_image(image_path, model, save_path):
 
 if __name__ == "__main__":
     # Example usage:
-    image_path = "/Users/manedavtyan/PycharmProjects/compvisionproject/data/test/PNEUMONIA/person1_virus_9.jpeg"
+    image_path = '../data/test/NORMAL/IM-0075-0001.jpeg'
 
-    save_path = "../result/1_result.txt" #"results/inference_result.txt"
+    save_path = "../result/IM-0075-0001_result.txt"
 
     # Instantiate the VGG16 model
     vgg16_model = SimpleVGG16()
 
     # Load the trained weights (replace 'path/to/your/model_weights.pth' with the actual path)
-    vgg16_model.load_state_dict(torch.load('../result/simple_vgg16.pth')) #'path/to/your/model_weights.pth'
+    vgg16_model.load_state_dict(torch.load('../result/simple_vgg16.pth'))
 
     # Run inference and save the result
     inference_single_image(image_path, vgg16_model, save_path)
